9ce4175f9f9a91d1c96e333c27ef83442d7fcb58
Changxu Luo <camelboat@sjtu.edu.cn>
2018-02-25 12:23:40 +0800

    new file:   Graphics.sh

---
+++ Graphics.sh
@@ -0,0 +1,19 @@
+# This is script for installing NVIDIA graphics
+# card driver to Ubuntu
+
+# First need to kill x server
+# Then install the driver
+
+# Driver is downloaded from NVIDIA official
+# support website
+
+##################################################
+#Please rename your driver filename as driver.run#
+##################################################
+
+sudo service lightdm stop
+sudo sh driver.run
+
+# If you want to automatically reboot your computer
+# Please uncomment the following line
+# sudo reboot
