9d3d90e4a619c7c4ab0927cf9fd42c51147cd33e
Changxu Luo <camelboat@sjtu.edu.cn>
2018-02-25 12:26:34 +0800

    modified:   Unlock.sh

--- Unlock.sh
+++ Unlock.sh
@@ -1,10 +1,13 @@
 # This is the Ubuntu shell script
+
 # When facing the following error message, you can run
 # this script to continue your installing process
-# ##
-# Could not get lock /var/lib/dpkg/lock
-# Unable to lock the administration directory (/var/lib/dpkg/)
-# ##
+
+# #############################################################
+# Could not get lock /var/lib/dpkg/lock                       #
+# Unable to lock the administration directory (/var/lib/dpkg/)#
+# #############################################################
+
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
