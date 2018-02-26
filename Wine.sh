be42e8a95dc933b2a8be3a826cc182170a887c53
Travis <camelboat@sjtu.edu.cn>
2018-02-26 17:39:46 +0800

    modified:   Wine.sh

--- Wine.sh
+++ Wine.sh
@@ -1,4 +1,9 @@
+# This is Ubuntu shell script for installing Wine
 sudo add-apt-repository ppa:wine/wine-builds
 sudo apt-get update
 sudo apt-get install --install-recommends wine-staging
 sudo apt-get install winehq-staging
+
+# If you want to remove Wine, please run the following commands
+# sudo apt-get install ppa-purge
+# sudo ppa-purge ppa:wine/wine-builds
