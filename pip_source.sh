6545e46d22186a7aa91933f9a16debc94637fdf9
camelboat <camelboat@sjtu.edu.cn>
2018-04-10 20:36:09 +0800

    modified:   pip_source.sh

--- pip_source.sh
+++ pip_source.sh
@@ -3,7 +3,11 @@
 # This is bash script running under Ubuntu 16.04 LTS
 # It changes the pip source to tsinghua pip source mirror
 
+# If you don't have directory of ~/.pip then run the following line
+# mkdir ~/.pip
+
 touch ~/.pip/pip.conf
 
 echo "[global]" >> ~/.pip/pip.conf
 echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple/" >> ~/.pip/pip.conf
+echo "pip source changing complete! :)"
