2abb62d174ca03db66d4d1a8ae8e2ca782cf48c7
camelboat <camelboat@sjtu.edu.cn>
2018-12-07 23:25:00 +0800

    new file:   Bluetooth.sh

---
+++ Bluetooth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# for more information, please check https://www.jaredwolff.com/get-started-with-bluetooth-low-energy/
+# This commands can allow bluetooth low energy on Linux
+# such as bluetooth mouse or headphones
+sudo btmgmt le on
